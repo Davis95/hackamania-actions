1014f0f3ec9981d76ea092040c95a75eee78c42a
paul-davis-sp <paul.davis@sailpoint.com>
2020-04-23 14:09:53 -0500

    send slack message

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ const axios = require('axios').default;
 
 console.log(actionsCore.getInput('comment'));
 console.log(github.context.payload);
-// axios.post("https://hooks.slack.com/services/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")
+axios.post("https://hooks.slack.com/services/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", 
+{
+    text: "hello",
+    channel: "#hackamania"
+},
+{
+    headers: {
+        'Content-type': 'application/json'
+    }
+}
+)
