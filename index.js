c470a2765c50d8f1b8414aab8441424e20243ae6
paul-davis-sp <paul.davis@sailpoint.com>
2020-04-23 13:48:25 -0500

    testing action

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const actionsCore = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios').default;
+
+
+console.log(actionsCore.getInput('comment'));
+console.log(github.context.payload);
+// axios.post("https://hooks.slack.com/services/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")
